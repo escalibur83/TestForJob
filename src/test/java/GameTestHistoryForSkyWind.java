9664d6dcf1996b8bfa61c7a2b5ebe5fec5e7dddc
Alexandr Garanovich <esc_blr@mail.ru>
2019-10-01 22:13:57 +0300

    Copy text fron Help page

--- src/test/java/GameTestHistoryForSkyWind.java
+++ src/test/java/GameTestHistoryForSkyWind.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class GameTestHistoryForSkyWind {
@@ -6,8 +8,16 @@ public class GameTestHistoryForSkyWind {
     public static void main (String[] args) throws InterruptedException {
 
         WebDriver chromeDriver = new ChromeDriver();
-        chromeDriver.get("https://www.onliner.by/");
-        Thread.sleep(1000);
+        chromeDriver.get("http://gc.gaming.skywindgroup.com/mammothmillions/develop/396/index.html");
+        Thread.sleep(10000);
+        chromeDriver.wait();
+        WebElement infoButton = chromeDriver.findElement(By.xpath(".//*[@ class = \'footer-active-element footer-icon footer-help-icon menu-help menu-disabling-sensitive\']"));
+        infoButton.click();
+        Thread.sleep(500);
+        WebElement infoText = chromeDriver.findElement(By.xpath(".//*[@ class = \"window-content\"]"));
+        String text = infoText.getText();
+        Thread.sleep(500);
         chromeDriver.quit();
+        System.out.println(text);
     }
 }
